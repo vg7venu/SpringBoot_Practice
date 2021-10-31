67d6ca3ffc311ad33804aeeaad18d24f9ac67918
vg7venu <vg7venu@gmail.com>
2021-10-31 17:00:35 +0530

    Added Logback

--- src/main/java/com/springed/demopackage/cdi/CDIImp.java
+++ src/main/java/com/springed/demopackage/cdi/CDIImp.java
@@ -3,9 +3,6 @@ package com.springed.demopackage.cdi;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 // @Component
 @Named
 public class CDIImp {
