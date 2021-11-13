9bb1c99c3cb4e9d6fcae5d414687fbdadb403174
vg7venu <vg7venu@gmail.com>
2021-11-13 21:29:51 +0530

    Init Spring AOP (Aspect Oriented Programming)

---
+++ aopexample/src/test/java/com/springboot/aopexample/aopexample/AopexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.aopexample.aopexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
